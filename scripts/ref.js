7d0b6b5f8ff1c8dabb0ee42d80d76158cd57abe0
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2024-03-19 23:48:51 +0930

    redo

--- scripts/ref.js
+++
@@ -1,4 +0,0 @@
-
-// stable-diffusion-webui\javascript\extraNetworks.js
-var dropPageSel = gradioApp().querySelector('#' + tabname + '_extra_page_numbers'); // INJECT HERE
-tabs.appendChild(dropPageSel); // INJECT HERE
